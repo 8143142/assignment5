c8a81f7e0a9de2f681a764f1c72dfe2fa9b47c6c
Nauriz <tasbula03@mail.ru>
2023-05-21 16:34:57 +0600

    method put done

--- src/BST.java
+++ src/BST.java
@@ -1,5 +1,9 @@
-public class BST<K extends Comparable<K>, V>{
+import java.util.ArrayList;
+import java.util.List;
+
+public class BST<K extends Comparable<K>, V> {
     private Node root;
+
     private class Node {
         private K key;
         private V val;
@@ -10,19 +14,23 @@ public class BST<K extends Comparable<K>, V>{
             this.val = val;
         }
     }
-    /*
-    public void put(K key, V val){
 
-    }
-    public V get(K key){
-
-    }
-    public void delete(K key){
-
-    }
-    public Iterable<K> iterator(){
-
+    public void put(K key, V val) {
+        root = put(root, key, val);
     }
 
-     */
+    private Node put(Node node, K key, V val) {
+        if (node == null) {
+            return new Node(key, val);
+        }
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = put(node.left, key, val);
+        } else if (cmp > 0) {
+            node.right = put(node.right, key, val);
+        } else {
+            node.val = val;
+        }
+        return node;
+    }
 }
