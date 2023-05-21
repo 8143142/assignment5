373162ad32b2d9ebf19fefab6c7a646d2d659b2b
Nauriz <tasbula03@mail.ru>
2023-05-21 21:04:08 +0600

    main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        BST<Integer, String> tree = new BST<>();
 
+        tree.put(5, "Arsen");
+        tree.put(3, "Oraz");
+        tree.put(7, "Rasul");
+        tree.put(2, "Rakhat");
+
+        for (BST.Node node : tree.iterator()) {
+            System.out.println("Key is " + node.key + " and value is " + node.val);
+        }
+
+        System.out.println();
+        tree.delete(7);
+
+        for (BST.Node node : tree.iterator()) {
+            System.out.println("Key is " + node.key + " and value is " + node.val);
+        }
     }
 }
